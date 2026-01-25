f398f4bdaabb5ed789488e781d219493aaa9d228
joshuarbt <joshua@raboteau.fr>
2026-01-26 00:46:38 +0100

    add age calculation script and update hero section to display age dynamically

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+
+// script age 
+const birthDate = new Date(2007, 4, 8);
+
+function getAge(date) {
+    const today = new Date();
+    let age = today.getFullYear() - date.getFullYear();
+
+    if (today.getMonth() > date.getMonth() || (today.getMonth() === date.getMonth() && today.getDate() >= date.getDate())) {
+        return age;
+    }else {
+        return age - 1;
+    }
+}
+
+document.getElementById("age").textContent = getAge(birthDate);
